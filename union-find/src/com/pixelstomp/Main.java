0efe66cbc020aadb0b40e6123d82ed75d89bc2f8
poplopo <aethereal@gmail.com>
2018-08-02 08:56:01 -0700

    Updated Quick Union functionality

--- union-find/src/com/pixelstomp/Main.java
+++ union-find/src/com/pixelstomp/Main.java
@@ -9,17 +9,27 @@ public class Main {
 	// write your code here
 
     Scanner scanner = new Scanner(System.in);
-    QuickFind qf;
 
-    System.out.println("Now evaluating QuickFind algorithm. Please enter the number of nodes you would like to instantiate.");
-    qf = new QuickFind(scanner.nextInt());
-
-    qf.doQuickFind(scanner);
+    startQuickUnion(scanner);
 
     }
 
-    private static void startQuickFind() {}
+    private static void startQuickFind(Scanner scanner) {
+        QuickFind qf;
 
-    private static void startQuickUnion() {}
+        System.out.println("Now evaluating QuickFind algorithm. Please enter the number of nodes you would like to instantiate.");
+        qf = new QuickFind(scanner.nextInt());
+
+        qf.doQuickFind(scanner);
+    }
+
+    private static void startQuickUnion(Scanner scanner) {
+        QuickUnion qu;
+
+        System.out.println("Now evaluating QuickUnion algorithm. Please enter the number of nodes you would like to instantiate.");
+        qu = new QuickUnion(scanner.nextInt());
+
+        qu.doQuickUnion(scanner);
+    }
 
 }
--- union-find/src/com/pixelstomp/QuickUnion.java
+++ union-find/src/com/pixelstomp/QuickUnion.java
@@ -1,5 +1,7 @@
 package com.pixelstomp;
 
+import java.util.Scanner;
+
 /**
  * This class will represent a Weighted quick union solution with O(lg n) complexity.
  */
@@ -7,6 +9,7 @@ public class QuickUnion {
 
     int[] id;
     int[] size;
+    static final String INSTRUCTIONS = "Press J to join, B to bulk-join, or F to find. Or press Q to exit.";
 
     public QuickUnion(int count) {
         id = new int[count];
@@ -19,15 +22,31 @@ public class QuickUnion {
 
     public void join(int p, int q) {
         // Performs root first
-        if (root(p) == root(q)) {
+        int pRoot = root(p);
+        int qRoot = root(q);
+        if (pRoot == qRoot) {
             return;
         }
 
         // Check the size
+        if (size[pRoot] < size[qRoot]) {
+            // join p to q's root
+            id[pRoot] = qRoot;
+            size[qRoot] += size[pRoot];
+            size[pRoot] = 0;
+        } else {
+            // join q to p's root
+            id[qRoot] = pRoot;
+            size[pRoot] += size[qRoot];
+            size[qRoot] = 0;
+        }
+
     }
 
     public void joinBulk(int[][] joined) {
-        
+        for (int i = 0; i < joined.length; i++) {
+            join(joined[i][0], joined[i][1]);
+        }
     }
 
     public boolean find(int p, int q) {
@@ -40,6 +59,46 @@ public class QuickUnion {
             return j;
         } else {
             return root(id[j]);
+        }
+    }
+
+    public void doQuickUnion(Scanner scanner) {
+        boolean on = true;
+
+        while (on == true) {
+            System.out.println(INSTRUCTIONS);
+            String next = scanner.next();
+
+            if (next.equals("Q")) {
+                on = false;
+            }
+
+            if (next.equals("J")) {
+                System.out.println("Enter the two nodes you would like joined, separated by carriage returns.");
+                int p = scanner.nextInt();
+                int q = scanner.nextInt();
+
+                this.join(p, q);
+
+                System.out.println("Nodes successfully joined.");
+            }
+
+            if (next.equals("B")) {
+                System.out.println("We'll need to create an array.");
+                System.out.println("The code for this has not been written yet.");
+            }
+
+            if (next.equals("F")) {
+                System.out.println("Enter two nodes to see if they are connected, separated by carriage return.");
+                int p = scanner.nextInt();
+                int q = scanner.nextInt();
+
+                boolean connected = this.find(p, q);
+
+                System.out.print("Connection found: ");
+                System.out.print(connected);
+                System.out.println();
+            }
         }
     }
 }
