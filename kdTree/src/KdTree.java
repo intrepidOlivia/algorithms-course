e5dc3affd1ba622112a2756e06af3ceb96072d9a
Marisha Parker <m_parker2@wargaming.net>
2018-09-06 17:01:11 -0700

    Set up initial files for KD Tree assignment

--- kdTree/src/KdTree.java
+++ kdTree/src/KdTree.java
@@ -1,18 +1,52 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
 
 public class KdTree {
 
-    public         KdTree() {}                               // construct an empty set of points
-    public           boolean isEmpty() {}                     // is the set empty?
-    public               int size() {}                        // number of points in the set
-    public              void insert(Point2D p) {}             // add the point to the set (if it is not already in the set)
-    public           boolean contains(Point2D p) {}           // does the set contain point p?
-    public              void draw() {}                        // draw all points to standard draw
-    public Iterable<Point2D> range(RectHV rect) {}            // all points that are inside the rectangle (or on the boundary)
-    public           Point2D nearest(Point2D p) {}            // a nearest neighbor in the set to point p; null if the set is empty
+    SET<Node> nSet;
 
-    private class Node {}
+    public KdTree() {
+        nSet = new SET<>();
+    }
+
+    public boolean isEmpty() {}                     // is the set empty?
+    public int size() {}                        // number of points in the set
+    public void insert(Point2D p) {}             // add the point to the set (if it is not already in the set)
+    public boolean contains(Point2D p) {}           // does the set contain point p?
+    public void draw() {}                        // draw all points to standard draw
+    public Iterable<Point2D> range(RectHV rect) {}            // all points that are inside the rectangle (or on the boundary)
+    public Point2D nearest(Point2D p) {}            // a nearest neighbor in the set to point p; null if the set is empty
+
+    private class Node implements Comparable<Node> {
+        Point2D point;
+        Node left = null;
+        Node right = null;
+        boolean isHorizontal;
+
+        Node(Point2D point, boolean isHorizontal) {
+            this.point = point;
+            this.isHorizontal = isHorizontal;
+        }
+
+        int compareTo(Node that) {
+            if (isHorizontal) {
+                // compare y values
+                double thisY = this.point.y();
+                double thatY = that.point.y();
+                if (thisY < thatY) {
+                    return -1;
+                }
+                if (thisY == thatY && this.point.x() == that.point.x()) {
+                    return 0;
+                }
+                return 1;
+            } else {
+                // compare x values
+            }
+            return 0;
+        }
+    }
 
     public static void main(String[] args) {}                 // unit testing of the methods (optional)
 }
--- kdTree/src/PointSET.java
+++ kdTree/src/PointSET.java
@@ -1,6 +1,8 @@
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class PointSET {
 
@@ -13,13 +15,76 @@ public class PointSET {
         pSet = new SET<>();
     }
 
-    public           boolean isEmpty() {}                      // is the set empty?
-    public               int size() {}                         // number of points in the set
-    public              void insert(Point2D p) {}              // add the point to the set (if it is not already in the set)
-    public           boolean contains(Point2D p) {}            // does the set contain point p?
-    public              void draw() {}                         // draw all points to standard draw
-    public Iterable<Point2D> range(RectHV rect) {}             // all points that are inside the rectangle (or on the boundary)
-    public           Point2D nearest(Point2D p) {}             // a nearest neighbor in the set to point p; null if the set is empty
+    public boolean isEmpty() {
+        return pSet.isEmpty();
+    }
 
-    public static void main(String[] args) {}                  // unit testing of the methods (optional)
+    public int size() {
+        return pSet.size();
+    }
+
+    public void insert(Point2D p) {
+        if (p == null) { throw new IllegalArgumentException(); }
+        pSet.add(p);
+    }
+
+    public boolean contains(Point2D p) {
+        if (p == null ) { throw new IllegalArgumentException(); }
+        return pSet.contains(p);
+    }
+
+    /**
+     * draws all points to standard draw
+     */
+    public void draw() {
+        for (Point2D p : pSet) {
+            StdDraw.point(p.x(), p.y());
+        }
+    }
+
+    /**
+     * Retrieves all points that are inside the specified rectangle
+     * @param rect The rectangle to check for points within
+     * @return iterable collection of points inside or on the boundary of the rectangle
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) { throw new IllegalArgumentException(); }
+        Stack<Point2D> included = new Stack<>();
+        for (Point2D p : pSet) {
+            if (rect.contains(p)) {
+                included.push(p);
+            }
+        }
+        return included;
+    }
+
+    /**
+     * @param p The point to find the nearest neighbor of
+     * @return The nearest neighbor to the specified point
+     */
+    public Point2D nearest(Point2D p) {
+        if (p == null) { throw new IllegalArgumentException(); }
+        if (isEmpty()) { return null; }
+
+        double distance = 0;
+        Point2D nearest = null;
+        for (Point2D compare : pSet) {
+            if (p.equals(compare)) { continue; }
+            double cDistance = p.distanceTo(compare);
+            if (distance == 0 || cDistance < distance) {
+                distance = cDistance;
+                nearest = compare;
+            }
+        }
+        return nearest;
+    }
+
+
+    public static void main(String[] args) {
+        StdDraw.setPenRadius(0.05);
+        StdDraw.point(0, 0);
+        StdDraw.point(1, 0);
+        StdDraw.point(0, 1);
+        StdDraw.point (1, 1);
+    }
 }
